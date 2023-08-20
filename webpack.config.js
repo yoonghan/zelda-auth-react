8b37d35b3657febf57078d986a5bee56a5f8bc76
han <walcoorperation@gmail.com>
2023-08-21 06:57:22 +0800

    Add html page

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
 
@@ -11,5 +12,10 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "src/index.html",
+      }),
+    ],
   });
 };
