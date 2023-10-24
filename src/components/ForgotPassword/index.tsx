b97feb349e80d5c461bab31b1f2b916696dea248
han <walcoorperation@gmail.com>
2023-10-24 18:30:37 +0800

    Fix forgot password ui display.

--- src/components/ForgotPassword/index.tsx
+++ src/components/ForgotPassword/index.tsx
@@ -52,7 +52,7 @@ export default function ForgotPassword({
         }}
       >
         <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}></Avatar>
-        <Typography component="h1" variant="h4">
+        <Typography component="h1" variant="h5">
           Reset a forgotten password
         </Typography>
         {renderedDisplay}
