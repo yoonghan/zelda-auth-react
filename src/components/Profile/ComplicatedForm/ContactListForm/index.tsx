47de8cbf835521c4423182231411a4e22619ea71
han <walcoorperation@gmail.com>
2023-11-16 01:07:09 +0800

    Allow to accept list of contacts into component.

--- src/components/Profile/ComplicatedForm/ContactListForm/index.tsx
+++ src/components/Profile/ComplicatedForm/ContactListForm/index.tsx
@@ -31,16 +31,28 @@ const generateContact = (id: string): ContactWithValid => ({
   isValid: false,
 })
 
-export default function ContactListForm() {
+interface Props {
+  listOfContacts?: Contact[]
+}
+
+export default function ContactListForm({ listOfContacts }: Props) {
   const { handleSubmit } = useForm()
 
-  const initialVal = 0
+  const initialVal = listOfContacts?.length || 0
   const [nextVal, setNextVal] = useState(initialVal + 1)
   const [total, setTotal] = useState(0)
   const [saveDialogOpen, setSaveDialogOpen] = useState(false)
   const [formValid, setFormIsValid] = useState(false)
 
-  const contacts = useRef([generateContact(`-new-${initialVal}`)])
+  const initialContacts = () => {
+    if (listOfContacts === undefined || listOfContacts.length === 0) {
+      return [generateContact(`-new-${initialVal}`)]
+    } else {
+      return listOfContacts.map((contact) => ({ ...contact, isValid: true }))
+    }
+  }
+
+  const contacts = useRef(initialContacts())
 
   const addItem = useCallback(() => {
     setNextVal(nextVal + 1)
